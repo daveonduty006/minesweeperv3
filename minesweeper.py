0e02b2de5dc37c606cba278e734c4aa56b16cb4b
David Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-05-02 21:03:32 -0400

    updated

--- minesweeper.py
+++ minesweeper.py
@@ -9,7 +9,6 @@ class Minesweeper:
         self.clicked_tiles = 0
         self.safe_tiles = 0
         self.board = self.create_board()
-        #print(self.board)
 
     def menu(self):
         print("CHOOSE THE BOARD DIMENSION: ")
@@ -24,58 +23,107 @@ class Minesweeper:
             print("3. HARD")
             sel = int(input("CHOICE: "))
         if sel == 1:
-            bomb_prob = 0.1
+            bomb_prob = 0.25
         elif sel == 2:
-            bomb_prob = 0.2
+            bomb_prob = 0.4
         else:
-            bomb_prob = 0.3
+            bomb_prob = 0.5
         return dim, bomb_prob  
 
     def start_game(self):
         running = True
         while running:
-            pass   
+            self.display()
+            #i = input(which row...)
+            #j = input(which column...)
+            #check = self.make_move(i,j)
+            #if not check:
+                #self.game_over = True
+            #if check:
+                #reveal tile 
+            pass 
+
+    def make_move(self, i, j):
+        if self.board[i][j] == "☼":
+            return False
+        elif self.board[i][j].mine_around > 0:
+            return True
+        pass
+
+    def display(self):
+        for row in self.board:
+            for data in row:
+                print(f"{data}", end ="")
+            print()
 
     def create_board(self):
         self.board = []
         alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.board.append([])
-        for i in range(self.boardsize[1]+1):
-                self.board[0].append(f" {i}")
+        for j in range(self.boardsize[1]+1):
+                self.board[0].append(f" {j}")
         self.board[0][0] = " "
         for i in range(1, self.boardsize[0]+1):
             self.board.append([])
             self.board[i].append(alphabet[i-1])
-            for j in range(self.boardsize[1]):
+            for j in range(1, self.boardsize[1]+1):
                 hide_bomb = random() < self.prob
                 if not hide_bomb:
                     self.safe_tiles += 1
-                tile = Tile(hide_bomb) 
+                tile = Tile(hide_bomb)
                 self.board[i].append(tile)
-        return self.board  
+        self.find_tile_neighbors()
+        return self.board
 
-    def display(self):
-        for line in self.board:
-            for data in line:
-                print(f"{data}", end ="")
-            print()
+    def find_tile_neighbors(self):
+        for i in range(1, self.boardsize[0]+1):
+            for j in range(1, self.boardsize[1]+1):
+                tile = self.board[i][j]
+                neighbors = self.get_tile_neighbors( (i,j) )
+                tile.set_neighbors(neighbors)
+
+    def get_tile_neighbors(self, tile_pos):
+        neighbors = []
+        for i in range(tile_pos[0]-1, tile_pos[0]+2):
+            for j in range(tile_pos[1]-1, tile_pos[1]+2):
+                # This boolean will be False if one neighbor of the tile doesn't exist
+                out_of_bounds = i < 1 or i >= self.boardsize[0]+1 or\
+                                j < 1 or j >= self.boardsize[1]+1
+                # This boolean will be True if the neighbor is actually the tile itself 
+                same = (i == tile_pos[0]) and (j == tile_pos[1])
+                if same or out_of_bounds:
+                    continue
+                neighbors.append(self.board[i][j])
+        return neighbors
 
 class Tile:
 
     def __init__(self, hide_bomb):
-        self.hide_boom = hide_bomb
+        self.hide_bomb = hide_bomb
         self.neighbors = []
+        self.mine_around = 0
         self.clicked = False
         self.flagged = False
 
-    def __repr__(self):
-        return f"Tile({self.hide_boom})"
+    def set_neighbors(self, neighbors_list):
+        # This method receives a list of neighbors from self.board (Minesweeper-object attribute)
+        self.neighbors = neighbors_list
+        print(self.neighbors)
+        for neighbor in self.neighbors:
+            if neighbor.hide_bomb:
+                self.mine_around += 1
+        print(self.mine_around)
 
     def __str__(self):
-        bomb_str = "☼" if self.hide_boom else "◙"
+        bomb_str = "☼" if self.hide_bomb else "◙"
         return f" {bomb_str}"
+
+    def __repr__(self):
+        return f"Tile({self.hide_bomb})"
+
 
 
     
 M = Minesweeper()
+#M.start_game()
 M.display()
