e724b2d01f27e124a9c736c288e7649f850dc866
daveonduty006 <dvdnormans@hotmail.com>
2022-05-11 19:31:04 -0400

    replace main.py class menu for main_menu function

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from board import Board 
 
-
 # The main module, this where the top-level code for the game will be executed.
 # Once the program is launched, a menu is looped back between each rounds.
 # If the player selects option 1, a new minesweeper board is instanciated and we 
 # enter the game loop. 
-
-class Menu:
-
+def main_menu():
     exit = False
     while not exit:
         sel = 0
@@ -42,6 +39,6 @@ class Menu:
 
 
 if __name__ == "__main__":
-    Menu()
+    main_menu()
 
         
